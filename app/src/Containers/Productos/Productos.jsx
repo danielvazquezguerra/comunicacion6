59ab1e747fe0840dc366993ecb664ff1c41faaf9
danielvazquezguerra <daniel.vazquezguerra@gmail.com>
2020-07-07 12:49:33 +0200

    bootstrap install

--- app/src/Containers/Productos/Productos.jsx
+++ app/src/Containers/Productos/Productos.jsx
@@ -34,16 +34,49 @@ const Productos = (props) => {
 
                     <h4>Branding</h4>
 
-                    <p>Las máquinas que poseemos nos permitirán dar un servicio de calidad y a un precio
-                    muy competitivo, que combinado con un excelente servicio y el valor agregado del
-                    diseño, será, la formula perfecta para marcar la diferencia y tener clientes satisfechos.</p>
+                    <p> Construimos tu marca mediante la administración 
+                        estratégica del conjunto total de activos vinculados en forma directa o indirecta al nombre 
+                        y/o símbolo (logotipo) influyendo en el valor de la marca, tanto 
+                        para el cliente como para tu empresa</p>
 
                 </div>
 
                 
                 <div className="ServiceItem">
 
-                    <h4>Branding</h4>
+                    <h4>Advertising</h4>
+
+                    <p> Definimos un conjunto de estrategias orientadas a dar a conocer productos y servicios a la sociedad. 
+                        Sin embargo, no solo se trata de que las personas sepan que existes, sino también de otorgar cierta 
+                        reputación a lo que se ofrece. Sin utilizar esta comunicación patrocinada, sería muy complejo posicionarse
+                        en la mente de los consumidores como algo de buena calidad.</p>
+
+                </div>
+
+                <div className="ServiceItem">
+
+                    <h4>Packaging</h4>
+
+                    <p> Creamos el concepto y diseño del empaque o envase de tu producto, convirtiendo el consumo en una experiencia
+                        única ty enfocada para tus clientes, acompañando a la marca que representas y dandole el valor que necesita 
+                        para difundir tu producto.
+                    </p>
+
+                </div>
+
+                <div className="ServiceItem">
+
+                    <h4>Sings</h4>
+
+                    <p> Diseñamos sistemas de comunicación visual sintetizado en un conjunto de señales o símbolos que cumplen la función de guiar, 
+                        orientar u organizar a una persona o conjunto de personas en aquellos puntos del espacio que planteen dilemas de 
+                        comportamiento, como por ejemplo dentro de una gran superficie.</p>
+
+                </div>
+
+                <div className="ServiceItem">
+
+                    <h4>Impresión</h4>
 
                     <p>Las máquinas que poseemos nos permitirán dar un servicio de calidad y a un precio
                     muy competitivo, que combinado con un excelente servicio y el valor agregado del
@@ -53,47 +86,20 @@ const Productos = (props) => {
 
                 <div className="ServiceItem">
 
-                    <h4>Branding</h4>
 
-                    <p>Las máquinas que poseemos nos permitirán dar un servicio de calidad y a un precio
-                    muy competitivo, que combinado con un excelente servicio y el valor agregado del
-                    diseño, será, la formula perfecta para marcar la diferencia y tener clientes satisfechos.</p>
+                    <h4>Diseño Gráfico</h4>
 
-                </div>
-
-                <div className="ServiceItem">
-
-                    <h4>Branding</h4>
-
-                    <p>Las máquinas que poseemos nos permitirán dar un servicio de calidad y a un precio
-                    muy competitivo, que combinado con un excelente servicio y el valor agregado del
-                    diseño, será, la formula perfecta para marcar la diferencia y tener clientes satisfechos.</p>
-
-                </div>
-
-                <div className="ServiceItem">
-
-                    <h4>Branding</h4>
-
-                    <p>Las máquinas que poseemos nos permitirán dar un servicio de calidad y a un precio
-                    muy competitivo, que combinado con un excelente servicio y el valor agregado del
-                    diseño, será, la formula perfecta para marcar la diferencia y tener clientes satisfechos.</p>
-
-                </div>
-
-                <div className="ServiceItem">
-
-                    <h4>Branding</h4>
-
-                    <p>Las máquinas que poseemos nos permitirán dar un servicio de calidad y a un precio
-                    muy competitivo, que combinado con un excelente servicio y el valor agregado del
-                    diseño, será, la formula perfecta para marcar la diferencia y tener clientes satisfechos.</p>
+                    <p> Tenemos el equipo necesario para imprimir a grandes formatos y en el material que necesite tu proyecto.
+                        Estar presente en superficies de gran tamaño puede llamar la atención de transeuntes y conductores completando
+                        tu plan publicitario. </p>
 
                 </div>
 
 
 
             </div>
+
+           
             
         </div>
     )
