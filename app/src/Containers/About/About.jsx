1e0f19cf78ccfbe8babc0d1692fcca71101f61ea
danielvazquezguerra <daniel.vazquezguerra@gmail.com>
2020-10-14 08:43:07 +0200

    about us mejoras

--- app/src/Containers/About/About.jsx
+++ app/src/Containers/About/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
 
                 <div className="AboutLeft">
 
-                    <img src="/images/about-comunicacion-6.png" alt=""/>
+                    <img src="/images/lapiz-macro.png" alt=""/>
 
 
 
