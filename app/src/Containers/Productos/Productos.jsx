fc9b79e7e6e7ada93c385c04a4b37e3ae27b0c5c
danielvazquezguerra <daniel.vazquezguerra@gmail.com>
2020-10-22 22:26:07 +0200

    iconos en productos adheridos

--- app/src/Containers/Productos/Productos.jsx
+++ app/src/Containers/Productos/Productos.jsx
@@ -23,6 +23,12 @@ const Productos = () => {
 
                     <h4>BRANDING</h4>
                     <p><span>Construimos tu marca</span> mediante la administración estratégica del conjunto total de activos vinculados en forma directa o indirecta al nombre y/o símbolo (logotipo), influyendo en el valor de la marca, tanto para el cliente como para su empresa</p>
+                    
+                    <div className="icon-content">
+                        <img src="/images/branding-icon-01.png" alt=""/>
+                        <img src="/images/branding-icon-02.png" alt=""/>
+                        <img src="/images/branding-icon-03.png" alt=""/>
+                    </div>
 
                </div>
                    
@@ -42,6 +48,12 @@ const Productos = () => {
                     <h4>IMPRESIÓN</h4>
                     <p>Las máquinas que poseemos nos permitirán dar un servicio de calidad y a un precio muy competitivo, que combinado con un excelente servicio y el valor agregado del diseño, será, la formula perfecta para marcar la diferencia y tener clientes satisfechos.</p>
 
+                    <div className="icon-content">
+                        <img src="/images/imprent-icon-01.png" alt=""/>
+                        <img src="/images/imprent-icon-02.png" alt=""/>
+                        <img src="/images/imprent-icon-03.png" alt=""/>
+                    </div>
+
                 </div>
                     
 
@@ -58,6 +70,12 @@ const Productos = () => {
 
                     <h4>PACKAGING</h4>
                     <p>Creamos el concepto y diseño del empaque o envase de tu producto, convirtiendo el consumo en una experiencia única y enfocada para tus clientes, acompañando a la marca que representas y dándole el valor que necesita para difundir tu producto.</p>
+
+                    <div className="icon-content">
+                        <img src="/images/packaging-icon-01.png" alt=""/>
+                        <img src="/images/packaging-icon-02.png" alt=""/>
+                        <img src="/images/packaging-icon-03.png" alt=""/>
+                    </div>
 
                 </div>
     
@@ -76,6 +94,12 @@ const Productos = () => {
 
                     <h4>ADVERTISING</h4>
                     <p>Definimos un conjunto de estrategias orientadas a dar a conocer productos y servicios a la sociedad, sin embargo, no sólo se trata de que las personas sepan que existes, sino también de otorgar cierta reputación a lo que se ofrece. Sin utilizar esta comunicación patrocinada, sería complejo posicionarse en la mente de los consumidores como algo de buena calidad.</p>
+
+                    <div className="icon-content">
+                        <img src="/images/hola-icon-01.png" alt=""/>
+                        <img src="/images/hola-icon-02.png" alt=""/>
+                        <img src="/images/hola-icon-03.png" alt=""/>
+                    </div>
 
                 </div>
 
@@ -96,6 +120,12 @@ const Productos = () => {
                     <h4>SIGNS</h4>
                     <p>Diseñamos sistemas de comunicación visual sintetizado en un conjunto de señales o símbolos que cumplen la función de guiar, orientar u organizar a una persona o grupos en aquellos puntos del espacio que planteen dilemas de comportamiento, como por ejemplo dentro de una gran superficie.</p>
 
+                    <div className="icon-content">
+                        <img src="/images/signs-icon-01.png" alt=""/>
+                        <img src="/images/signs-icon-02.png" alt=""/>
+                        <img src="/images/signs-icon-03.png" alt=""/>
+                    </div>
+
                 </div>
 
 
@@ -105,7 +135,7 @@ const Productos = () => {
 
                 <div className="ProductosMainLeft">
 
-                    <img src="/images/design-mockup.jpg" alt="" />
+                    <img src="/images/design-mockup-2.jpg" alt="" />
 
                 </div>
 
@@ -113,6 +143,12 @@ const Productos = () => {
 
                     <h4>DISEÑO GRÁFICO</h4>
                     <p>Tenemos el equipo necesario para imprimir a grandes formatos y en el material que necesite tu proyecto. Estar presente en superficies de gran tamaño puede llamar la atención de transeuntes y conductores completando tu plan publicitario.</p>
+
+                    <div className="icon-content">
+                        <img src="/images/graphic-icon-01.png" alt=""/>
+                        <img src="/images/graphic-icon-02.png" alt=""/>
+                        <img src="/images/graphic-icon-03.png" alt=""/>
+                    </div>
 
                 </div>
 
