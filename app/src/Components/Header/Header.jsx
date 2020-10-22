63204b20377085745fcd1a0eb5d8c0eed1222098
danielvazquezguerra <daniel.vazquezguerra@gmail.com>
2020-10-22 23:22:04 +0200

    botones arreglados

--- app/src/Components/Header/Header.jsx
+++ app/src/Components/Header/Header.jsx
@@ -14,9 +14,9 @@ const Header = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
       
-        <div className="ButtonsBox collapse navbar-collapse" id="navbarSupportedContent">
+        <div className="ButtonsBox navbar-collapse" id="navbarSupportedContent">
 
-          <ul className="Buttons navbar-nav mr-auto">
+          <ul className="Buttons navbar-nav">
             <li className="nav-item">
               <a className="nav-link" href="/">HOME <span className="sr-only">(current)</span></a>
             </li>
